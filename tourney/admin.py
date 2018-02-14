d24c2d93608fe6c6a7c00abcebf05b3fe3e1582a
Branko Bajcetic <bajcetic.branko@gmail.com>
2018-02-14 12:24:34 -0800

    Register models for admin page

--- tourney/admin.py
+++ tourney/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile,Tourney,Match
+
+admin.site.register(Profile)
+admin.site.register(Tourney)
+admin.site.register(Match)
