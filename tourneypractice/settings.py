f3cf2e24e5cd123b802f4b7e23eb69710a611207
Branko Bajcetic <bajcetic.branko@gmail.com>
2018-02-13 16:49:14 -0800

    Add tourney config to installed apps

--- tourneypractice/settings.py
+++ tourneypractice/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tourney.apps.TourneyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
