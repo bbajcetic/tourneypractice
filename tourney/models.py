419dc4fe2c88974c47a240179d65dd7e55035051
Branko Bajcetic <bajcetic.branko@gmail.com>
2018-02-13 17:05:49 -0800

    Update models and make them more readable

--- tourney/models.py
+++ tourney/models.py
@@ -4,22 +4,38 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, 
+            null=True, 
+            on_delete=models.CASCADE)
     wins = models.PositiveIntegerField(default=0)
 
 class Tourney(models.Model):
-    name = models.CharField(max_length=30,blank=True)
-    winner = models.ForeignKey(Profile, related_name='tourneywinner', blank=True, null = True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30,
+            blank=True,null=True)
+    winner = models.ForeignKey(Profile, 
+            related_name='tourneywinner', 
+            blank=True,null = True, 
+            on_delete=models.CASCADE)
     size = models.PositiveIntegerField()
+    player = models.ManyToManyField(Profile, 
+            blank=True,null=True)
 
 class Match(models.Model):
-    winner = models.ForeignKey(Profile, related_name='matchwinner', on_delete=models.CASCADE)
-    player1 = models.ForeignKey(Profile, related_name='player1', on_delete=models.CASCADE)
-    player2 = models.ForeignKey(Profile, related_name='player2', on_delete=models.CASCADE)
-    tourney = models.ForeignKey(Tourney, on_delete=models.CASCADE)
-
-class User(models.Model):
-    name = models.CharField(max_length=30,blank=True)
+    winner = models.ForeignKey(Profile, 
+            related_name='matchwinner', 
+            blank=True,null=True,
+            on_delete=models.CASCADE)
+    player1 = models.ForeignKey(Profile, 
+            blank=True,null=True, 
+            related_name='player1', 
+            on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Profile, 
+            blank=True,null=True, 
+            related_name='player2', 
+            on_delete=models.CASCADE)
+    tourney = models.ForeignKey(Tourney, 
+            blank=True,null=True,
+            on_delete=models.CASCADE)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender,instance,created,**kwargs):
